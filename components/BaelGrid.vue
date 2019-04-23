44dc811d878babe097a899a419adf431309741be
David Miner <miner49er98@gmail.com>
2019-04-22 22:33:50 -0400

    Updated homepage columns

--- components/BaelGrid.vue
+++ components/BaelGrid.vue
@@ -1,157 +1,182 @@
 <template>
-<div class="xs-text-6 md-text-5">
-    <div v-if="items2[0]" class="masonry browse" :style="`margin-top:${navbarheight}px`">
-      <div v-if="items2[pi]" v-for="(p,pi) in items2" :key="p.pi" class="xs-p2 masonry-brick item">
-        <h2 class="xs-my2 bold">{{p.title}}</h2>
-        <div v-html="p.body"></div>
-        <nuxt-link class="xs-my1 xs-text-center button button--pinterest bold" :to="p._path">
-          READ MORE
-        </nuxt-link>
+  <div class="xs-text-6 md-text-5" :style="`margin-top:${navbarheight}px`">
+    <div v-if="items2[0]" class="xs-py4 table grid clearfix">
+      <div class="xs-col-3 xs-float-left">
+        <div v-for="r in [1,2,3,4,5,6]" :key="r.pi">
+          <nuxt-link :to="`https://www.wikipedia.com`"><h3 class="xs-mb2 bold">Resource {{r}}</h3></nuxt-link>
+          <div>You wouldn't believe whats behind this link. It will connect you to the rest of the universe</div>
+        </div>
       </div>
-
+      <div class="xs-col-6 browse xs-float-left">
+        <div v-for="(p,pi) in items2" :key="p.pi" >
+          <div v-if="items2[pi]" class="xs-px2 item">
+            <h2 class="xs-my2 bold">{{p.title}}</h2>
+            <div v-html="p.body"></div>
+            <nuxt-link class="xs-my1 xs-text-right button button--transparent bold" :to="p._path">
+              <span class="text-red">READ MORE</span>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+      <div class="xs-col-3 xs-float-left">
+        <div v-for="r in [1,2,3]" :key="r.pi">
+          <h3 class="xs-mb2 bold">Sweet Data {{r}}</h3>
+          <div>This crazy data from somewhere, really shows us how Massachusetts is really taking a stance on this random subject that no one knows anything about. It really is amazing what some time and money can do to sway the general population!</div>
+          <img src="/images/uploads/random.png" />
+        </div>
+      </div>
     </div>
     <div v-else class="r full-height browse">
-      <div class="xs-p2 c-100 xs-flex xs-flex-align-center xs-flex-justify-center xs-text-center" :style="`height:calc(100vh - ${navbarheight}px);margin-top:${navbarheight}px`">
+      <div class="xs-p2 c-100 xs-flex xs-flex-align-center xs-flex-justify-center xs-text-center"
+        :style="`height:calc(100vh - ${navbarheight}px);margin-top:${navbarheight}px`">
         <div v-if="total < 1 && !busy">No Results.</div>
       </div>
-  </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  props: ["items", "allitems"],
-  data() {
-    return {
-      currentPage: null,
-      pageNumbers: [],
-      pageNumberCount: 0,
-      items2: [],
-      query: 1,
-      busy: false,
-      count: 0
-    };
-  },
-  methods: {
-    pageCheck() {
-      if (this.allitems.length > 6) {
-        this.$store.commit("paginateOn", true);
-        this.$store.commit("resultsLength", this.allitems.length);
-      } else if (this.allitems.length < 6) {
-        this.$store.commit("paginateOff", false);
-      } else {
-        this.$store.commit("paginateOff", false);
-      }
+  export default {
+    props: ["items", "allitems"],
+    data() {
+      return {
+        currentPage: null,
+        pageNumbers: [],
+        pageNumberCount: 0,
+        items2: [],
+        query: 1,
+        busy: false,
+        count: 0
+      };
     },
-
-    loadMore() {
-      this.count = this.offset;
-      if (this.total > this.count && this.busy == false) {
-        this.busy = true;
-        this.items2.splice(0);
-
-        for (var i = 0, j = 6; i < j; i++) {
-          const api = this.allitems[this.count];
-
-          this.items2.push(api);
-          this.count++;
+    methods: {
+      pageCheck() {
+        if (this.allitems.length > 6) {
+          this.$store.commit("paginateOn", true);
+          this.$store.commit("resultsLength", this.allitems.length);
+        } else if (this.allitems.length < 6) {
+          this.$store.commit("paginateOff", false);
+        } else {
+          this.$store.commit("paginateOff", false);
         }
+      },
 
-        this.busy = false;
-      }
-    },
+      loadMore() {
+        this.count = this.offset;
+        if (this.total > this.count && this.busy == false) {
+          this.busy = true;
+          this.items2.splice(0);
 
-    onResize(event) {
-      this.navHeight();
-    },
+          for (var i = 0, j = 6; i < j; i++) {
+            const api = this.allitems[this.count];
 
-    navHeight() {
-      if (process.browser) {
-        var height = document.getElementById("navbar").clientHeight;
+            this.items2.push(api);
+            this.count++;
+          }
 
-        this.$store.commit("SET_NAVHEIGHT", height - 1);
-      }
-    }
-  },
-  watch: {
-    // whenever question changes, this function will run
-    $route({ params, query }) {
-      if (this.$route.query.page > 1) {
-        this.loadMore();
+          this.busy = false;
+        }
+      },
+
+      onResize(event) {
         this.navHeight();
-        this.pageCheck();
-      } else if (this.$route.query.page == null) {
-        this.$route.query.page = 1;
+      },
+
+      navHeight() {
+        if (process.browser) {
+          var height = document.getElementById("navbar").clientHeight;
+
+          this.$store.commit("SET_NAVHEIGHT", height - 1);
+        }
+      }
+    },
+    watch: {
+      // whenever question changes, this function will run
+      $route({
+        params,
+        query
+      }) {
+        if (this.$route.query.page > 1) {
+          this.loadMore();
+          this.navHeight();
+          this.pageCheck();
+        } else if (this.$route.query.page == null) {
+          this.$route.query.page = 1;
+          this.loadMore();
+          this.navHeight();
+          this.pageCheck();
+        } else {
+          this.loadMore();
+          this.navHeight();
+          this.pageCheck();
+        }
+      },
+      queryParam: function () {
         this.loadMore();
-        this.navHeight();
-        this.pageCheck();
-      } else {
-        this.loadMore();
-        this.navHeight();
-        this.pageCheck();
       }
     },
-    queryParam: function() {
-      this.loadMore();
-    }
-  },
-  computed: {
+    computed: {
 
-    offset() {
-      if (this.queryParam > 1) {
-        return Number(this.queryParam - 1) * 6;
-      } else {
-        return 0;
+      offset() {
+        if (this.queryParam > 1) {
+          return Number(this.queryParam - 1) * 6;
+        } else {
+          return 0;
+        }
+      },
+      prevpage() {
+        const prev = Number(this.queryParam) - 1;
+        return prev <= 1 ? 1 : prev;
+      },
+      nextpage() {
+        const next = Number(this.queryParam) + 1;
+        return next;
+      },
+      navbarheight() {
+        return this.$store.state.navheight;
+      },
+      total() {
+        return this.allitems.length;
+      },
+
+      queryParam() {
+        if (this.$route.query.page == null) {
+          return 1;
+        } else {
+          return Number(this.$route.query.page);
+        }
       }
     },
-    prevpage() {
-      const prev = Number(this.queryParam) - 1;
-      return prev <= 1 ? 1 : prev;
-    },
-    nextpage() {
-      const next = Number(this.queryParam) + 1;
-      return next;
-    },
-    navbarheight() {
-      return this.$store.state.navheight;
-    },
-    total() {
-      return this.allitems.length;
-    },
 
-    queryParam() {
-      if (this.$route.query.page == null) {
-        return 1;
-      } else {
-        return Number(this.$route.query.page);
-      }
-    }
-  },
-
-  updated() {
-    this.$nextTick(() => {
-      this.pageCheck();
-      this.navHeight();
-      this.$store.commit("SET_GRIDOFFSET", this.offset);
-    });
-  },
-  mounted() {
-    if (process.browser) {
-      this.loadMore();
-
+    updated() {
       this.$nextTick(() => {
-        this.navHeight();
         this.pageCheck();
-        window.addEventListener("resize", this.onResize);
+        this.navHeight();
+        this.$store.commit("SET_GRIDOFFSET", this.offset);
       });
+    },
+    mounted() {
+      if (process.browser) {
+        this.loadMore();
+
+        this.$nextTick(() => {
+          this.navHeight();
+          this.pageCheck();
+          window.addEventListener("resize", this.onResize);
+        });
+      }
+    },
+    beforeDestroy() {
+      // Unregister the event listener before destroying this Vue instance
+      window.removeEventListener("resize", this.onResize);
     }
-  },
-  beforeDestroy() {
-    // Unregister the event listener before destroying this Vue instance
-    window.removeEventListener("resize", this.onResize);
-  }
-};
+  };
 </script>
 
 <style>
+.grid {
+  width: 1200px;
+  max-width: 100%;
+  margin: 0 auto;
+}
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,14 @@
-var siteInfo = require('./content/setup/info.json');
+import webpack from 'webpack'
+
+const siteInfo = require('./content/setup/info.json');
 console.log(siteInfo)
-var glob = require('glob');
-var path = require('path');
+const glob = require('glob');
+const path = require('path');
 
 // Enhance Nuxt's generate process by gathering all content files from Netifly CMS
 // automatically and match it to the path of your Nuxt routes.
 // The Nuxt routes are generate by Nuxt automatically based on the pages folder.
-var dynamicRoutes = getDynamicPaths({
+const dynamicRoutes = getDynamicPaths({
   '/blog': 'blog/posts/*.json',
   '/page': 'page/posts/*.json',
   '/category': 'categories/posts/*.json',
@@ -17,41 +19,57 @@ var dynamicRoutes = getDynamicPaths({
 module.exports = {
   mode: "universal",
   /*
-  ** Headers of the page
-  */
-transition: { mode: "in-out"},
-env: {
-  API_URL: process.env.API_URL,
-},
+   ** Headers of the page
+   */
+  transition: {
+    mode: "in-out"
+  },
+  env: {
+    API_URL: process.env.API_URL,
+  },
   head: {
     title: siteInfo.sitename,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: siteInfo.sitedescription }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: siteInfo.sitedescription
+      }
 
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Archivo+Black' }
+    link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Archivo+Black'
+      }
     ]
   },
-  css: ["@/assets/grid.css","bf-solid/dist/solid.latest.css"],
+  css: ["@/assets/grid.css", "bf-solid/dist/solid.latest.css"],
   // icon: {
   //   iconSrc: `${siteInfo.siteicon}`
   //  },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  modules: ['@nuxtjs/markdownit', '@nuxtjs/pwa','@nuxtjs/axios'],
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#3B8070'
+  },
+  modules: ['@nuxtjs/markdownit', '@nuxtjs/pwa', '@nuxtjs/axios'],
   markdownit: {
     injected: true,
     preset: 'default',
     breaks: true,
     html: true
-
-    
   },
   manifest: {
     name: siteInfo.sitename,
@@ -61,46 +79,53 @@ env: {
   },
   workbox: {
     fetch: true,
-    runtimeCaching: [
-      {
-        urlPattern: '/images/uploads/.*',
-        handler: 'cacheFirst',
-        strategyOptions: {
-          cacheName: 'image-cache',
-          cacheExpiration: {
-            maxEntries: 100,
-            maxAgeSeconds: 86400
-          }
+    runtimeCaching: [{
+      urlPattern: '/images/uploads/.*',
+      handler: 'cacheFirst',
+      strategyOptions: {
+        cacheName: 'image-cache',
+        cacheExpiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 86400
         }
       }
-    ]
+    }]
   },
 
   /*
-  ** Route config for pre-rendering
-  */
- router: {
-  scrollBehavior: function (to, from, savedPosition) {
-    return { x: 0, y: 0 }
+   ** Route config for pre-rendering
+   */
+  router: {
+    scrollBehavior: function (to, from, savedPosition) {
+      return {
+        x: 0,
+        y: 0
+      }
+    },
+    middleware: ['title']
   },
-middleware: ['title']
- },
   generate: {
     routes: dynamicRoutes
   },
-  plugins: ['~/plugins/vuefuse',{
+  plugins: ['~/plugins/vuefuse', {
     src: "~/plugins/moment",
     ssr: false
   }],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
-    extractCSS: true
+    extractCSS: true,
     /*
-    ** Run ESLint on save
-    */
+     ** Run ESLint on save
+     */
 
+    plugins: [
+      new webpack.ProvidePlugin({
+        '_': 'lodash'
+        // ...etc.
+      })
+    ]
   }
 }
 
@@ -113,7 +138,9 @@ function getDynamicPaths(urlFilepathTable) {
     ...Object.keys(urlFilepathTable).map(url => {
       var filepathGlob = urlFilepathTable[url];
       return glob
-        .sync(filepathGlob, { cwd: 'content' })
+        .sync(filepathGlob, {
+          cwd: 'content'
+        })
         .map(filepath => `${url}/${path.basename(filepath, '.json')}`);
     })
   );
