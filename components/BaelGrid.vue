181a48120f9a3d75105a3e58cfc8283f40bab3af
David Miner <miner49er98@gmail.com>
2019-04-24 00:27:07 -0400

    Display content consistently

--- components/BaelGrid.vue
+++ components/BaelGrid.vue
@@ -7,7 +7,7 @@
           <nuxt-link :to="r._path">
             <h3 class="xs-mb2 bold">{{r.title}}</h3>
           </nuxt-link>
-          <div>{{r.body}}</div>
+          <div v-html="r.body"></div>
         </div>
       </div>
       <div class="xs-col-6 xs-px4 browse xs-float-left">
@@ -26,7 +26,7 @@
         <Title text="Popular" />
         <div v-for="s in popular" :key="s.pi">
           <h3 class="xs-mb2 bold">{{s.title}}</h3>
-          <div>{{s.body}}</div>
+          <div v-html="s.body"></div>
           <img v-if="s.featuredImage" class="xs-py1" :src="`${s.featuredImage}`" />
         </div>
       </div>
